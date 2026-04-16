f79c74fdbdfe7b78fa5de7173daa41ec6b36e693
t-kitajima <t-kitajima@CD26305L.arksystems.co.jp>
2026-04-16 16:37:44 +0900

    lesson8が完了

--- Java100_questions_cac2nd/src/lesson08/challenge02/Nursery/NurserySchool.java
+++ Java100_questions_cac2nd/src/lesson08/challenge02/Nursery/NurserySchool.java
@@ -1,19 +1,6 @@
-/**
- * 第8章 保育園のお仕事
- *
- * 問題1  ファイルの分割
- *
- * 保育士クラス（Nurse）と
- * 園児クラス（NurserySchoolChild）と
- * 保育園クラス（NurserySchool）を別のファイルに
- * 記述し直してください。
- *
- */
-
-package lesson08.challenge01;
+package lesson08.challenge02.Nursery;
 
 public class NurserySchool {
-
 	public static void main(String[] args) {
 
 		Nurse nurse1 = new Nurse("保育太郎");
@@ -32,5 +19,4 @@ public class NurserySchool {
 		System.out.println(child2.getName() + "が登園しました。");
 		System.out.println(child3.getName() + "が登園しました。");
 	}
-
 }
--- Java100_questions_cac2nd/src/lesson08/challenge03/Nurserry/main/NurserySchool.java
+++ Java100_questions_cac2nd/src/lesson08/challenge03/Nurserry/main/NurserySchool.java
@@ -1,19 +1,9 @@
-/**
- * 第8章 保育園のお仕事
- *
- * 問題1  ファイルの分割
- *
- * 保育士クラス（Nurse）と
- * 園児クラス（NurserySchoolChild）と
- * 保育園クラス（NurserySchool）を別のファイルに
- * 記述し直してください。
- *
- */
+package lesson08.challenge03.Nurserry.main;
 
-package lesson08.challenge01;
+import lesson08.challenge03.Nurserry.logic.Nurse;
+import lesson08.challenge03.Nurserry.logic.NurserySchoolChild;
 
 public class NurserySchool {
-
 	public static void main(String[] args) {
 
 		Nurse nurse1 = new Nurse("保育太郎");
@@ -32,5 +22,4 @@ public class NurserySchool {
 		System.out.println(child2.getName() + "が登園しました。");
 		System.out.println(child3.getName() + "が登園しました。");
 	}
-
 }
