94ee8dffad08bdd46f6485b36f371091d5c22af9
t-kitajima <t-kitajima@CD26305L.arksystems.co.jp>
2026-04-17 13:45:21 +0900

    Lesson9-4内のerror処理を追記しました

--- Java100_questions_cac2nd/src/lesson09/challenge04/Nurserry/Exception/CriticalConditionException.java
+++ Java100_questions_cac2nd/src/lesson09/challenge04/Nurserry/Exception/CriticalConditionException.java
@@ -1,4 +1,31 @@
 package lesson09.challenge04.Nurserry.Exception;
 
 public class CriticalConditionException extends Exception {
+
+	public CriticalConditionException() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public CriticalConditionException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public CriticalConditionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public CriticalConditionException(String message) {
+		super(message);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public CriticalConditionException(Throwable cause) {
+		super(cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
 }
